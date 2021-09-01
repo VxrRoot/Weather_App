243bc16d0d8c5812b7e2f8c2e94f76c91b89f380
Dawid S <dawidslowik379@gmail.com>
2021-09-01 18:20:06 +0200

    set default location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 const api = {
   key: "c4a6a31cba6df404911f210abdbd3212",
   base: "https://api.openweathermap.org/data/2.5/",
@@ -16,7 +16,6 @@ function App() {
         .then((result) => {
           setQuery("");
           setWeather(result);
-          console.log(result);
         });
     }
   };
@@ -54,6 +53,19 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
 
+  const defaultWeather = () => {
+    fetch(`${api.base}weather?q=krosno&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setQuery("");
+        setWeather(result);
+      });
+  };
+
+  useEffect(() => {
+    defaultWeather();
+  }, []);
+
   return (
     <div
       className={
@@ -76,21 +88,23 @@ function App() {
           />
         </div>
         {typeof weather.main !== "undefined" ? (
-          <div>
-            <div className="location-box">
-              <div className="location">
-                {weather.name}, {weather.sys.country}
+          <div className="wrapper">
+            <div className="location_div">
+              <div className="location-box">
+                <div className="location">
+                  {weather.name}, {weather.sys.country}
+                </div>
+                <div className="date">{dateBuilder(new Date())}</div>
               </div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°C</div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather-box">
+                <div className="temp">{Math.round(weather.main.temp)}°C</div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
             </div>
           </div>
         ) : (
           <div className="location-box">
-            <div className="location">Wrong Location, try again</div>
+            <div className="location">Check weather in your city</div>
           </div>
         )}
       </main>
